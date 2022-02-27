1f2daf34a68378a77fe017244b2757d1c46ea5ad
pdahal48 <prem.dahal01@gmail.com>
2022-02-27 14:01:42 -0500

    footer configured

--- Backend/kuti-seed.sql
+++ Backend/kuti-seed.sql
@@ -38,11 +38,11 @@ VALUES
 INSERT INTO lahenga 
   (id, seller_username, name, material, used, description, sale, price, sale_price, color, waist_size, length, size)
 VALUES
-  (1, 'tbusiness1', 'Traditional Lahenga', 'silk', FALSE, 'traditional saree made from silk perfect for you', TRUE, 22.50, 15.49, 'red', 28, 25, 4),
-  (2, 'tbusiness2', 'Designer Embroidery Lahengas', 'cotton', FALSE, 'Traditional saree made from silk perfect for you', FALSE, 32.50, 22.50, 'blue', 32, 21, 5),
-  (3, 'tbusiness3', 'Turquoise Embroidered Art Silk', 'silk', FALSE, 'Traditional saree made from silk perfect for you', FALSE, 45.50, 45.50, 'red', 28, 29, 2),
-  (4, 'tbusiness4', 'White Embroidered', 'silk', FALSE, 'Traditional saree made from silk perfect for you', FALSE, 85.50, 75.50, 'red', 40, 32, 4),
-  (5, 'tbusiness5', 'Elegant yellow colored', 'silk', FALSE, 'Traditional saree made from silk perfect for you', FALSE, 68.80, 68.80, 'yellow', 18, 39, 3);
+  (1, 'tbusiness1', 'Traditional Lahenga', 'silk', FALSE, 'traditional Lahenga made from silk perfect for you', TRUE, 22.50, 15.49, 'red', 28, 25, 4),
+  (2, 'tbusiness2', 'Designer Embroidery Lahengas', 'cotton', FALSE, 'Traditional Lahenga made from silk perfect for you', FALSE, 32.50, 22.50, 'blue', 32, 21, 5),
+  (3, 'tbusiness3', 'Turquoise Embroidered Art Silk', 'silk', FALSE, 'Traditional Lahenga made from silk perfect for you', FALSE, 45.50, 45.50, 'red', 28, 29, 2),
+  (4, 'tbusiness4', 'White Embroidered', 'silk', FALSE, 'Traditional Lahenga made from silk perfect for you', FALSE, 85.50, 75.50, 'red', 40, 32, 4),
+  (5, 'tbusiness5', 'Elegant yellow colored', 'silk', FALSE, 'Traditional Lahenga made from silk perfect for you', FALSE, 68.80, 68.80, 'yellow', 18, 39, 3);
 
 INSERT INTO jwelery 
   (id, seller_username, name, material, used, description, sale, price, sale_price, color, brand, occassion, image, size)
